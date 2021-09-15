8af0d4d6d8d134cc8fd213fea11ecf935c0b767a
Heena Bhavnani <heena.bhavnani@ugamsolutions.com>
2021-09-15 16:25:56 +0530

    Sling models added

---
+++ core/src/main/java/com/personalproject/core/models/TimelineImpl.java
@@ -0,0 +1,52 @@
+package com.personalproject.core.models;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Model(
+        adaptables = SlingHttpServletRequest.class,
+        adapters = Timeline.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+
+public class TimelineImpl implements Timeline{
+
+    private static final Logger LOG = LoggerFactory.getLogger(FactAreaImpl.class);
+
+
+    @Inject
+    Resource componentResource;
+
+    @Override
+    public List<Map<String, String>> getTimelineDetailsWithMap() {
+        List<Map<String, String>> timelineDetailsMap=new ArrayList<>();
+        try {
+            Resource timelineDetail=componentResource.getChild("timelinedetailswithmap");
+            if(timelineDetail!=null){
+                for (Resource timeline : timelineDetail.getChildren()) {
+                    Map<String,String> timelineMap=new HashMap<>();
+                    timelineMap.put("heading",timeline.getValueMap().get("heading",String.class));
+                    timelineMap.put("title",timeline.getValueMap().get("title",String.class));
+                    timelineMap.put("text",timeline.getValueMap().get("text",String.class));
+                    timelineDetailsMap.add(timelineMap);
+                }
+            }
+        }catch (Exception e){
+            LOG.info("\n ERROR while getting Fact Details {} ",e.getMessage());
+        }
+
+        LOG.info("\n SIZE {} ",timelineDetailsMap.size());
+        return timelineDetailsMap;
+    }
+
+}
