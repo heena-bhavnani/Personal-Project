2a2c57445a3537d30268522a16ec2b2ccf15bea7
Heena Bhavnani <heena.bhavnani@ugamsolutions.com>
2021-11-23 17:09:25 +0530

    JUnit Testing

--- core/src/main/java/com/personalproject/core/models/HomeBannerImpl.java
+++ core/src/main/java/com/personalproject/core/models/HomeBannerImpl.java
@@ -1,46 +1,32 @@
 package com.personalproject.core.models;
 
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
-
-@Model(
-        adaptables = SlingHttpServletRequest.class,
+@Model(adaptables = SlingHttpServletRequest.class,
         adapters = HomeBanner.class,
+        //resourceType = HomeBannerImpl.RESOURCE_TYPE,
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
 public class HomeBannerImpl implements HomeBanner {
-
-//    final protected static String RESOURCE_TYPE = "personalproject/components/content/homebannerarea";
-
-    @Inject
-    Resource resource;
-
-    @Inject
-    @Via("resource")
-    String bio;
-
-    @Inject
-    @Via("resource")
-    String fullname;
-
+    private static final Logger LOG = LoggerFactory.getLogger(HomeBannerImpl.class);
+    //final protected static String RESOURCE_TYPE="personalproject/components/content/homebanner";
     @ValueMapValue
-    String mybutton;
-
+    private String bio;
     @ValueMapValue
-    String intro;
-
+    private String fullname;
     @ValueMapValue
-    String img;
-
+    private String intro;
     @ValueMapValue
-    String pathfield;
-
+    private String mybutton;
+    @ValueMapValue
+    private String pathfield;
+    @ValueMapValue
+    private String img;
     @Override
     public String getBio() {
         return bio;
@@ -62,12 +48,14 @@ public class HomeBannerImpl implements HomeBanner {
     }
 
     @Override
-    public String getHeroImage() {
-        return img;
-    }
-
-    @Override
     public String getPath() {
         return pathfield;
     }
+
+    @Override
+    public String getHeroImage() {
+        return img;
+    }
 }
+
+
